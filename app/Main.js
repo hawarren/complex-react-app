8cf333703d2bad487838d2291382d7bb59c93de1
Hanif Warren <Hanif.warren@gmail.com>
2024-07-16 18:00:39 -0400

    Finished 19 Routing (Single Page Application)

--- app/Main.js
+++ app/Main.js
@@ -5,14 +5,21 @@ import ReactDOM from "react-dom/client"
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
 import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function Main() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Routes>
+        <Route path="/" element={<HomeGuest />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
