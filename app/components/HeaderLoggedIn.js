ef221eb6d3d5df68407770ae56ab2cdad50c5a81
Hanif Warren <Hanif.warren@gmail.com>
2024-08-01 18:40:49 -0400

    Finished 41 Use Context

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react"
+import React, { useContext, useEffect } from "react"
 import { Link } from "react-router-dom"
+import ExampleContext from "../ExampleContext"
 
 function HeaderLoggedIn(props) {
+  const { setLoggedIn } = useContext(ExampleContext)
   function handleLogout() {
-    props.setLoggedIn(false)
+    setLoggedIn(false)
     localStorage.removeItem("complexappToken")
     localStorage.removeItem("complexappUsername")
     localStorage.removeItem("ComplexAppvatar")
